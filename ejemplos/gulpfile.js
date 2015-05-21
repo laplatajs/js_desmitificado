c1aace2ebcc31ecfb25d4620950484e295f7e834
Martín Acosta <tinchoz49@gmail.com>
2015-05-21 08:38:18 -0300

    saco browserify para poder usar el window como corresponde

--- ejemplos/gulpfile.js
+++ ejemplos/gulpfile.js
@@ -1,4 +1,19 @@
-/**
- * Gulp tasks to work with browserify, reworkcss and others great tools
- */
-require('./gulp-appfy')(__dirname);
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+// use default task to launch BrowserSync and watch JS files
+gulp.task('serve', function () {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    // add browserSync.reload to the tasks array to make
+    // all browsers reload after tasks are complete.
+    gulp.watch("src/*.js", browserSync.reload);
+});
+
+gulp.task('default', ['serve']);
